17daaaa41373d70f63cf0dd7b27545106ae192c3
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-10 11:09:21 +0100

    update

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,10 +1,11 @@
-from brownie import Lottery, accounts, config, networks
+from brownie import Lottery, accounts, config, network
 from web3 import Web3
 
 
 def test_get_entrace_fee():
     account = accounts[0]
-    lottery = Lottery.deploy(config['networks'][networks.show_active()]['eth_usd_price_feed'], {'from': account},
+    lottery = Lottery.deploy(
+        config['networks'][network.show_active()]['eth_usd_price_feed'],{'from': account},
     )
     assert lottery.getEntraceFee() > Web3.toWei(0.018, 'ether')
     assert lottery.getEntraceFee() < Web3.toWei(0.022, 'ether' )
