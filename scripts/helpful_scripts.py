b0e656b4def31e3409ea9e07cd51b2e2aaa042ae
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-14 15:03:57 +0100

    update

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,8 @@
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_fey"])
