1b62d67f4e3c427d31da093a88c0c1e25e127d91
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-14 17:04:26 +0100

    update

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,8 +1,12 @@
+from brownie import accounts, network, config
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet_fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
 def get_account():
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_fey"])
+    return accounts.add(config["wallets"]["from_fey"])
