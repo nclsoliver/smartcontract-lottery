0c2dd38ad7c32db03c1e08c18c74587ff9416e68
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-15 11:50:08 +0100

    adding get_contract function

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,8 +1,12 @@
 from scripts.helpful_scripts import get_account
+from brownie import Lottery
 
 
 def deploy_lottery():
     account = get_account()
+    lottery = Lottery.deploy(
+        get_contract('eth_usd_price_feed')
+    )
     
 def main():
     deploy_lottery()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,12 +1,63 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, MockV3Aggregator, Contract
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet_fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
-def get_account():
+
+def get_account(index=None, id=None):
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
     return accounts.add(config["wallets"]["from_fey"])
+
+
+contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator}
+
+
+def get_contract(contract_name):
+    """This function will grab the contract address from the brownie config if defined, otherwise, it will deploy a mock version of that contract, and return that mock contract
+
+    Args:
+        contract_name (string)
+
+    returns:
+        brownie.network.contract.ProjectContract: The most recently deployed version of this contract.
+        for example: MockV3Aggregator[-1]"""
+
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            # MockV3Agreggator.length
+            deploy_mocks()
+        contract = contract_type[-1]
+        # MockV3Aggregator[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        # address
+        # ABI
+        contract = Contract.from_abi(
+            contract_type.name, contract_address, contract_type.abi
+        )
+        # MockV3Aggregator.abi
+    return contract
+
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
+
+
+def deploymocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    mock_price_feed = MockV3Aggregator.deploy(
+        decimals, initial_value, {"from": account}
+    )
+    print("Deployed!")
