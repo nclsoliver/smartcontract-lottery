14f2121f5107a10c7f5ff3a1b6106cdd34b9626a
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-16 15:22:26 +0100

    complementing the function VRFCoordinator

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,12 @@
-from brownie import accounts, network, config, MockV3Aggregator, Contract, VRFCoordinatorMock, LinkToken
+from brownie import (
+    accounts,
+    network,
+    config,
+    MockV3Aggregator,
+    Contract,
+    VRFCoordinatorMock,
+    LinkToken,
+)
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet_fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
@@ -21,9 +29,10 @@ def get_account(index=None, id=None):
 
 
 contract_to_mock = {
-    'eth_usd_price_feed': MockV3Aggregator,
-    'vrf_coordinator': VRFCoordinatorMock,
-    'link_token': LinkToken,}
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
 
 
 def get_contract(contract_name):
@@ -58,8 +67,20 @@ DECIMALS = 8
 INITIAL_VALUE = 200000000000
 
 
-def deploymocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     account = get_account()
-    MockV3Aggregator.deploy(decimals, initial_value, {"from": account}
-    )
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deployed!")
+
+
+def fund_with_link(
+    contract_address, accouunt=None, link_token=None, amount=100000000000000000
+):  # 0.1 Link
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    tx.wait(1)
+    print('Fund contract!')
+    return tx
